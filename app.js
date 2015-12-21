020dc3ff986a107afdd47bf2b982a8fef6d85818
sumantav19 <sumantav19@yahoo.in>
2015-12-21 17:24:11 +0530

    Added contenful node app

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-var Client = require('node-rest-client').Client;
-var client = new Client();
 
-client.get('https://cdn.contentful.com/spaces/a075i0mc3urv?access_token=92a696fdb7ed270f1a9751f991400eaa9532bf3f819b20abb0ea86bcd137ceaf',function(data,response){
-					console.log(data);
-					return data;
-				},function(err){
-					console.log(err);
-				});
+
+var contentful = require('contentful');
+var client = contentful.createClient({
+	space : 'a075i0mc3urv',
+	accessToken : '25eaaf272b79ec778c8ff0b90f55e9dfb9b0507ad78044db262bf800f09f2e0f'
+})
+
+client.entry('Mw98VtQp0cy02Ys4iwyQu')
+	.then(function  (entry) {
+		// body...
+		return console.log(entry+'data');
+	},function(err){
+		console.log('error')
+	});
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,16 @@
 var gulp = require('gulp'), gulpjade = require('gulp-jade') , gulpData = require('gulp-data');
 
-var Client = require('node-rest-client').Client;
-var client = new Client();
+
 
 gulp.task('templates',function () {
 	// body...
 	
 		gulp.src (['./src/jade/*.jade'])
 		.pipe(gulpData(function(file){
-			return (function(){
-				client.get('https://cdn.contentful.com/spaces/a075i0mc3urv?access_token=92a696fdb7ed270f1a9751f991400eaa9532bf3f819b20abb0ea86bcd137ceaf',function(data,response){
-					console.log(data);
-					return data;
-				},function(err){
-					console.log(err);
-				})
-			}());
+			return require('./src/json/index.json');
+		}))
+		.pipe(gulpData(function(file){
+			return require('./src/json/entries.json')
 		}))
 		.pipe(gulpjade( {pretty: true}))
 		.pipe(gulp.dest('./dest'));
